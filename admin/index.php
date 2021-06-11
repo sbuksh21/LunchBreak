937d1e13b41ae5b3dae9bc881c79f7f0a649ac4b
sbuksh21 <sara_im@live.com>
2021-06-12 00:08:00 +0300

    commit

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title> LunchBreak.net - Home </title>
+
+    <link rel = "stylesheet" href = "">
+</head>
+
+<body>
+<!--- Menu starts here -->
+<div class = "menu">
+    Menu Goes here
+</div>
+<!--- Menu ends here -->
+
+<!--- Main starts here -->
+
+<div class = "main-contents">
+
+
+<!--- Menu ends here -->
