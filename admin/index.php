14311dc6143259d6ad9f4a7d50b637a280c45689
sbuksh21 <sara_im@live.com>
2021-06-12 01:02:49 +0300

    commit

--- admin/index.php
+++ admin/index.php
@@ -2,28 +2,34 @@
 <head>
     <title> LunchBreak.net - Home </title>
 
-    <link rel = "stylesheet" href = "">
+    <link rel = "stylesheet" href="../css/admin.css">
 </head>
 
 <body>
 <!--- Menu starts here -->
 <div class = "menu">
+    <div class = "wrapper">
     Menu Goes here
+</div>
 </div>
 <!--- Menu ends here -->
 
 <!--- Main starts here -->
 
 <div class = "main-contents">
+<div class = "wrapper">
     Main content goes here
+    </div>
     </div>
 <!--- Main ends here -->
 
 <!--- Footer starts here -->
     <div class = "footer">
-        Footer starts here hello
+    <div class = "wrapper">
+    <p class = "text-center"> Copyright <img src="https://img.icons8.com/emoji/15/000000/copyright-emoji.png"/> 2021 LunchBreak.net. All rights reserved.<p>
 <!-- Footer ends here --->
-
+</div>
+</div>
 </body>
 </html>
 
