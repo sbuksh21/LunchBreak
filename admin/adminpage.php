4362013386f775a9aa508a7546ece63452ca84a9
sbuksh21 <sara_im@live.com>
2021-06-12 14:21:15 +0300

    commit

---
+++ admin/adminpage.php
@@ -0,0 +1,31 @@
+<?php include('fixed/menu.php'); ?>
+
+<!--- Main starts here -->
+
+<div class = "main-content">
+<div class = "wrapper">
+    <h2> Admin Management</h2>
+
+    <table class = "tblfull">
+        <tr>
+            <th>S.N</th>
+            <th>Full Name</th>
+            <th>Username</th><th>
+            <th>Actions</th>
+        </tr>
+            <td>1.</td>
+            <td>Salwa Imam Buksh</td>
+            <td>sbuksh</td>
+            <td>
+                Update Admin
+                Delete Admin
+            </td>
+
+    </table>
+
+</div>
+</div>
+
+<!--- Main ends here -->
+
+<?php include('fixed/footer.php') ?>
