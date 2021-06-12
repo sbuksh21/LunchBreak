c89ad3e583a4aac56e741ae699fce4959fd4a9fa
sbuksh21 <sara_im@live.com>
2021-06-12 18:27:00 +0300

    commit

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -5,24 +5,30 @@
 <div class = "main-content">
 <div class = "wrapper">
     <h2> Admin Management</h2>
+    <br><br>
 
     <form acton=" " method = "POST">
 
     <table class = "tbl-2">
         <tr>
             <td>Full Name: </td>
-            <td> <input type= "text" name ="full name" placeholder="Enter Full Name" ></td>
+            <td> <input type= "text" name ="full_name" placeholder="Enter Full Name" required value ></td>
             </tr>
 
             <tr>
             <td>Username: </td>
-            <td> <input type= "text" name ="username" placeholder="Enter Username" ></td>
+            <td> <input type= "text" name ="username" placeholder="Enter Username" required value></td>
             </tr>
 
             <tr>
             <td>Password: </td>
-            <td> <input type= "password" name ="password" placeholder="Enter Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"></td>
+            <td> <input type= "password" name ="password" placeholder="Enter Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required value></td>
             </tr>
+
+            <tr>
+                <td colspan = "2">
+                    <input type = "submit" name = "submit" value ="Add Admin" class = "btn-secondary"> </td>
+            </tr> 
 
         </table>
 
@@ -31,5 +37,33 @@
 </div>
 </div>
 <?php include('fixed/footer.php') ?>
+
+<?php
+
+//Form processing and saving in database
+
+//To check whether the button is clicked or not
+
+if(isset($_POST['submit']))
+{
+    // Submiited
+ //1. Get value from form
+
+ $full_name = $_POST['full_name'];
+ $username = $_POST['username'];
+ $password = md5($_POST['password']); // Password encrypted with MD5
+
+ //2. Query to save data into database
+ $sql = "INSERT INTO tbl_admin SET 
+  full_name='$full_name',
+  username='$username',
+  passsword='$password'
+
+ ";
+
+}
+
+
+?>
 
 
---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+//Create constants 
+define('LOCALHOST', 'localhost'); 
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'lunchbreak');
+
+//Execute query to save data in database
+$conn = mysqli_connect(LOCALHOST , DB_USERNAME , DB_PASSWORD) or die(mysqli_error()); // Connect to the database
+$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Choosing database
+
+?>
