e506cc1e8122a9b325ff3658f01b7c3e5b3be7fa
sbuksh21 <sara_im@live.com>
2021-06-12 19:52:30 +0300

    commit

--- admin/add-admin.php
+++ admin/add-admin.php
@@ -62,20 +62,20 @@ if(isset($_POST['submit']))
  ";
 
 //3.Execute query to save data in database
- $res = mysqli_query($conn, $sql) or die(mysqli_error());
+ //$res = mysqli_query($conn, $sql) or die(mysqli_error());
 
 //4. Check whether the data is inserted or not and query has been executed
 
-if($res==TRUE)
+//if($res==TRUE)
 {
 //New Admin Registered
 echo "Registered";
 }
-else
+//else
 {
 //New Admin not Registered
 
-echo "Failed to register";
+//echo "Failed to register";
 }
 
 }
