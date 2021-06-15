21af7d6ffa75f7337f2da8b60de0e780377d1986
sbuksh21 <sara_im@live.com>
2021-06-15 20:42:30 +0300

    commit

---
+++ admin/adminlogin.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title> Admin Login - LunchBreak</title>
+        <link rel = "stylesheet" href = "../css/admin.css">
+    </head>
+
+    <body>
+
+    <div class = "login">
+        <h1>Login</h1>
+    </div>
+
+</body>
+</html>
+
+<?php include('fixed/footer.php') ?>
+
