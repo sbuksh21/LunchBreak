56b6dff02582940a267c148e2de5d4fac696e32c
sbuksh21 <sara_im@live.com>
2021-06-19 11:45:49 +0300

    commit

--- login.php
+++ login.php
@@ -1,3 +1,21 @@
+<?php
+// Start session
+session_start();
+
+//Create constants 
+define('SITEURL', 'http://localhost/LunchBreak/');
+define('LOCALHOST', 'localhost'); 
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'lunchbreak');
+
+// Connection 
+$conn = mysqli_connect(LOCALHOST , DB_USERNAME , DB_PASSWORD, DB_NAME) or die(mysqli_error()); // Connect to the database
+$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Choosing database
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,6 +50,8 @@
         </div>
     </section>
     <!-- Banner ends here-->
+
+   
 
     <!-- Login Starts here -->
     <div class = "container1">
