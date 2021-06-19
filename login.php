2a759f6f946c849bc99fb9d5e5f9bb790edc1292
sbuksh21 <sara_im@live.com>
2021-06-19 14:28:39 +0300

    commit

--- login.php
+++ login.php
@@ -1,22 +1,5 @@
 
-
-<?php
-// Start session
-session_start();
-
-//Create constants 
-define('SITEURL', 'http://localhost/LunchBreak/');
-define('LOCALHOST', 'localhost'); 
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'lunchbreak');
-
-// Connection 
-$conn = mysqli_connect(LOCALHOST , DB_USERNAME , DB_PASSWORD, DB_NAME) or die(mysqli_error()); // Connect to the database
-$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Choosing database
-
-?>
-
+<?php include('config/constants.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -44,16 +27,12 @@ $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Choosing
     </section>
 
     <!-- Navbar ends here-->
-
-    
+ 
     <!-- Banner starts here-->
     <section class="food-banner2">
         <div class ="container">
         
     <!-- Banner ends here-->
-
-   
-
     <!-- Login Starts here -->
     <div class = "container1">
         <form action = "#" class ="login">
@@ -70,7 +49,4 @@ $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //Choosing
 </div>
     </section>
         <!-- Login Ends here -->
-        <br> 
-        <br> <br>
-        <br>
-        <br>
+       
