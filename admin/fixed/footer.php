f561dc3b092ef5ac26feb6a4b38aafbeeb0cd2bc
sbuksh21 <sara_im@live.com>
2021-06-23 21:12:59 +0300

    commit

--- admin/fixed/footer.php
+++ admin/fixed/footer.php
@@ -1,6 +1,6 @@
 <!--- Footer starts here -->
-<div class = "footer">
-    <div class = "wrapper">
+<div class = "Footer3">
+    <div class = "footer-wrapper">
     <p class = "text-center"> Copyright <img src="https://img.icons8.com/ios-glyphs/15/ffffff/copyright.png"/> 2021 LunchBreak.net. All rights reserved.<p>
 <!-- Footer ends here --->
 </div>
