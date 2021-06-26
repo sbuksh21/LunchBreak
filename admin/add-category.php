24dc648498d80e4c9ae95fd7db4c36923b29422c
sbuksh21 <sara_im@live.com>
2021-06-27 01:39:50 +0300

    commit

--- admin/add-category.php
+++ admin/add-category.php
@@ -4,17 +4,147 @@
 
 <div class = "main-content">
 <div class = "wrapper">
-    <h2> Category Management</h2>
-    <br> <br>
+    <h2> Add  Category</h2>
+    
 
-      <!--- Button Add Category --->
-  <a href = "add-category.php" class = "btn-primary">Add Category</a>  
+        <?php
 
-<br>  <br>  <br>
+        if(isset($_SESSION['add']))
+        {
+            echo $_SESSION['add'];
+            unset($_SESSION['add']);
+        }
+
+        if(isset($_SESSION['upload_image']))
+        {
+            echo $_SESSION['upload_image'];
+            unset($_SESSION['upload_image']);
+        }
+
+        ?>
+<br> <br>
+      <!--- Adding a New Food Category --->
+ <form action = "" method = "POST" enctype="multipart/form-data">
+
+    <table class = "table">
+    <tr>
+        <td> Category Name : </td>
+        <td>
+           <input type="text" name = "cat_name" placeholder = "Category Name">
+        </td>
+    </tr>
+
+    <tr>
+        <td> Choose Picture: </td>
+        <td>
+           <input type="file" name = "image_name">
+        </td>
+    </tr>
+
+    <tr>
+        <td> Status: </td>
+        <td>
+           <input type="radio" name = "status" value = "Available"> Available
+           <input type="radio" name = "status" value = "Not Available"> Not Available
+        </td>
+    </tr>
+
+    <tr> 
+                <td colspan = "2"> <br>
+                    <input type = "submit" name = "submit" value ="Add Category" class = "btn-secondary"> </td>
+            </tr> 
+</table>
+</form>
+<!--- Adding a New Food Category --->
+
+<?php
+
+if(isset($_POST['submit']))
+{
+  
+    // Getting the data from form
+    $cat_name = mysqli_real_escape_string($conn, trim($_POST['cat_name']));
+
+
+    // Checking whether radio selection is chosen or not to avoid error
+    if(isset($_POST['status']))
+    {
+        $status = $_POST['status'];
+    }
+
+    else
+    {
+        // Setting the value as default 
+        $status = "Not Available";
+    }
+
+
+        //Image choosen or not
+     if(isset($_FILES['image_name']['name']))
+
+     {
+         //To upload the picture (to upload image path should be included)
+        $image_name =$_FILES['image_name']['name'];
+        $sourcepath = $_FILES['image_name']['tmp_name']; 
+        $destinationpath = "../Images/categories/".$image_name;
+    
+        //Successfully uploading the picture
+        $upload= move_uploaded_file($sourcepath, $destinationpath);
+    
+        if($upload==false)
+        {
+            //Display the message on failure
+            $_SESSION['upload_image'] = "<div class = 'failed'><b> Uploading Failed.</b> </div>";
+            header('location:'.SITEURL.'admin/add-category.php');
+            
+            // We will stop this process if we failed to insert the picture so we will not add the data into database
+                die();
+        }
+           
+        }
+
+    //Query to insert the data to db
+    $sql= "INSERT INTO tbl_category SET
+    cat_name='$cat_name' ,
+    image_name = '$image_name' ,
+    status='$status' , 
+    cat_date = NOW()
+    ";
+
+    //Executing query
+    $result = mysqli_query($conn, $sql);
+
+    // Checking query
+    if($result==true)
+    {
+        //on success
+        $_SESSION['add'] = "<div class = 'success'> <b>New Category Added. </b></div>";
+        //Redirecting
+        header('location:'.SITEURL. 'admin/admincategory.php');
+    }
+    else
+    {
+        //on fail
+        $_SESSION['add'] = "<div class = 'failed'><b> Failed to Add. </b></div>";
+        //Redirecting
+        header('location:'.SITEURL. 'admin/add-category.php');
+
+    }
+}
+
+
+?>
 
 
 
 </div>
 </div>
+            
+
+
+
+
+
+
 
 <?php include('fixed/footer.php') ?>
--- admin/admincategory.php
+++ admin/admincategory.php
@@ -7,26 +7,104 @@
     <h2> Category Management</h2>
     <br> <br>
 
+    <?php
+
+      if(isset($_SESSION['add']))
+      {
+          echo $_SESSION['add'];
+          unset($_SESSION['add']);
+      }
+
+    ?>
+    <br> <br>
+
+
       <!--- Button Add Category --->
-  <a href = "#" class = "btn-primary">Add Category</a>  
+  <a href = "add-category.php" class = "btn-primary">Add Category</a>  
 
 <br>  <br>  <br>
 
   <table class = "tblfull">
       <tr>
           <th>S.N</th>
-          <th>Full Name</th>
-          <th>Username</th>
-          <th>Actions</th>
+          <th>Category Name</th>
+          <th>Picture</th>
+          <th>Status</th>
+          <th>Date/Time</th>
+          <th>Action</th>
       </tr>
-          <td>1.</td>
-          <td>Salwa Imam Buksh</td>
-          <td>sbuksh</td>
-          <td>
-          <a href = "#" class = "btn-secondary">Update Admin</a> 
-          <a href = "#" class = "btn-danger">Delete Admin</a> 
-          </td>
-  
+
+      <?php
+
+      // Executing query, making connection and counting rows
+        $sql = "SELECT * FROM tbl_category";
+        $result = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($result);
+
+        if($count>0)
+        {
+
+          // If there is data in category 
+
+          while($rows=mysqli_fetch_assoc($result))
+          {
+            $id = $rows['id'];
+            $cat_name = $rows['cat_name'];
+            $image_name = $rows['image_name'];
+            $status = $rows['status'];
+            $cat_date = $rows['cat_date'];
+           
+
+            ?>
+
+            <tr>
+                      <td><?php echo $id ?></td>
+                      <td><?php echo $cat_name ?></td>
+
+
+                      <td>
+                      
+                        <?php 
+                        
+                        // Query to check image and display it 
+                        if($image_name!=="")
+
+                        {
+                          // To display the image, by going inside siteurl--images folder--categorires folder -- to fetch the image and display it
+                          ?>
+                          <img src="<?php echo SITEURL;?>Images/categories/<?php echo $image_name; ?>" width = "100px"> 
+                          <?php
+                        }
+                        else
+                        {
+                          echo "<div class = 'failed'><b> Picture is missing. </b></div>";
+                        }
+                        ?>
+                      </td>
+                      
+                      <td><?php echo $status ?></td>
+                      <td><?php echo $cat_date ?></td>
+                   
+                      <td>
+                      <a href = "" class = "btn-secondary">Update Category</a> 
+                      <a href = "delete-category.php" class = "btn-danger">Delete Category</a> 
+                      </td>
+                </tr>
+                <?php
+          }
+          
+        }
+        else
+        {
+          // If there is no data in category then below message will be displayed
+          ?>
+          <tr>
+            <td colspan="6"> <div class = "failed"><b> No Category to display.</b></div><td>
+            </tr>
+            <?php
+        }
+      ?>
+
   </table>
 </div>
 </div>
