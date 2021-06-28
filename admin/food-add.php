f58b5d39ae97526f59635faae5a12bcdf8685846
sbuksh21 <sara_im@live.com>
2021-06-28 22:21:19 +0300

    commit

--- admin/food-add.php
+++ admin/food-add.php
@@ -4,7 +4,9 @@
 
 <div class = "main-content">
 <div class = "wrapper">
-    <h2> Update Category</h2>
+    <h2> Add Food</h2>
+
+
 
 <br> <br>
 
@@ -21,7 +23,7 @@
     <tr>
         <td>Food Description:</td>
         <td>
-            <textarea name = "desc" cols = "28" rows = "6" placeholder = "Food Description"> </textarea>
+            <textarea name = "desc" cols = "21" rows = "6" placeholder = "Food Description"> </textarea>
         </td>
     </tr>
 
@@ -31,14 +33,145 @@
         </td>
     </tr>
 
+    <tr>
+        <td> Choose Image: </td>
+        <td> <input type = "file" name = "image">    
+        </td>
+    </tr>
+
+    <tr>
+        <td> Food Category: </td>
+        <td> 
+        <select name = "category">
+
+            <?php
+
+            // Displaying categories from db
+            // Displaying only available categories
+            $sql = "SELECT * FROM tbl_category WHERE status='Available' ";
+
+            $res = mysqli_query($conn, $sql);
+
+            $count = mysqli_num_rows($res); 
+
+            if($count>0)
+            {
+
+                while($rows=mysqli_fetch_assoc($res))
+                {
+                    $id = $rows['id'];
+                    $food_name = $rows['cat_name'];
+                    
+                    ?>
+
+                    <option value = "<?php echo $id; ?>"> <?php echo $food_name; ?> </option>
+
+                    <?php
+
+                }
+            }
+            else
+            {
+                ?>
+                <option value="0"> No Category Available </option>
+
+                <?php
+            }
+
+            ?>
+
+        </select>     
+        </td>
+    </tr>
+
+    <tr>
+        <td> Status: </td>
+        <td>
+           <input type="radio" name = "status" value = "Available" required value> Available
+           <input type="radio" name = "status" value = "Unavailable" required value> Unavailable
+        </td>
+    </tr>
+
+    <tr>
+        <td colspan= "2">
+            <input type = "submit" name = "submit" value = "Add Food" class= "btn-secondary">
+        </td>
+    </tr>
+
 </table>
 </form>
 
 
+<?php
 
+if(isset($_POST['submit']))
+{
+    $food_name = $_POST['food_name'];
+    $food_description = $_POST['desc'];
+    $price = $_POST['price'];
+    $food_category = $_POST['category'];
 
+    if(isset($_POST['status'])) // Checking the status option
+    {
+        $status = $_POST['status'];
+    }
+    else
+    {
+        $status = "Unavailable";
+    }
 
+    if(isset($_FILES['image']['name']))
+    {
+        $image_name = $_FILES['image']['name'];
 
+        if($image_name !="") //Checking whether Image is selected
+        {
+            $src = $_FILES['image']['tmp_name'];
+            $dst = "../Images/food/".$image_name;
+            $upload = move_uploaded_file($src, $dst);
+
+            //Checking whether imagae uploaded or not
+            if($upload==false)
+            {
+                $_SESSION['upload'] = "<div class = 'failed'> Failed to upload Image.</div>";
+                header('location:'.SITEURL.'admin/food-add.php'); //Redirecting
+                die(); // Stoping further processing
+            }
+        }
+    }
+    else
+    {
+        $image_name = "";
+    }
+
+    // Query to add New food 
+    $sql1 = "INSERT INTO tbl_food SET 
+    food_name = '$food_name',
+    desc = '$food_description',
+    price = $price , 
+    image_name = '$image_name',
+    category_id = $food_category,
+    status = '$status',
+    cat_date = NOW()
+    ";
+
+    //Query Execution
+    $res1 = mysqli_query($conn, $sql1);
+
+    if($res1==true)
+    {
+        $SESSION['add'] = "<div class = 'success'><b> New Food added successfuly.</b></div>";
+        header('location:'.SITEURL.'admin/adminfood.php'); // Redirecting to admin food page
+    }
+    else
+    {
+        $SESSION['add'] = "<div class = 'failed'><b> Failed to add new food.</b></div>";
+        header('location:'.SITEURL.'admin/adminfood.php'); // Redirecting to admin food page
+    }
+
+}
+
+?>
 
 
 </div>
